aa408e1c5aa537a42f52b4439ad2bbd4a8170388
Saksham Sharma <sakshamsharmaofficial7@gmail.com>
2024-06-17 04:03:41 +0530

    Home and About page Completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header className="header">
       <nav className="nav container">
         <a href="index.html" className="nav__logo">
-          Saksham
+          Saksham.
         </a>
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/velocityvortex_"
+        className="home__social-icon"
+        targer="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/saksham-sharma-07656524b/"
+        className="home__social-icon"
+        targer="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+      <a
+        href="https://github.com/Anonymous096"
+        className="home__social-icon"
+        targer="_blank"
+      >
+        <i class="uil uil-github"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
