575537c40d3833b917991a33520679b6c9d8edd7
Saksham Sharma <sakshamsharmaofficial7@gmail.com>
2024-06-17 07:37:43 +0530

    skills page Completed

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Backend Skills</h3>
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skill__name">PHP</h3>
+              <span className="skills_level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skill__name">NodeJs</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skill__name">Python</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="skills__group">
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skill__name">MySQL</h3>
+              <span className="skills_level">Advanced</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skill__name">Firebase</h3>
+              <span className="skills_level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skill__name">AWS</h3>
+              <span className="skills_level">Intermediate</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
