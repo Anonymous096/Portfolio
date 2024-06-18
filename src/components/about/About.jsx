4fbe172622c18268b996b59760b1c1d991ad51e6
Saksham Sharma <sakshamsharmaofficial7@gmail.com>
2024-06-18 11:33:47 +0530

    Header Update and scroll top added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
-import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/John-Cv.pdf";
+import AboutImg from "../../assets/mine.jpeg";
+import CV from "../../assets/MyResume.pdf";
 import Info from "./Info";
 
 const About = () => {
