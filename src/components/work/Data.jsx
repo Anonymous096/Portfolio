9fe4104ae31809ae5e88cf5c2ca254281faea954
Saksham Sharma <sakshamsharmaofficial7@gmail.com>
2024-07-08 17:41:16 +0530

    UI version Update

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,7 +1,7 @@
 import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
+import Work2 from "../../assets/Flowz.png";
 import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
+import Work4 from "../../assets/pdftalks.png";
 import Work5 from "../../assets/work5.jpg";
 
 export const projectsData = [
@@ -14,7 +14,7 @@ export const projectsData = [
   {
     id: 2,
     image: Work2,
-    title: "App movil",
+    title: "Flowz",
     category: "app",
   },
   {
@@ -26,7 +26,7 @@ export const projectsData = [
   {
     id: 4,
     image: Work4,
-    title: "App movil",
+    title: "PDF Talks",
     category: "app",
   },
   {
